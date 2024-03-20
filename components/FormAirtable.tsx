0dce665dad81817ae39f63f013eb21ec8254688c
Sergio Rey <reysergio383@gmail.com>
2024-03-19 23:46:27 -0500

    Update FormAirtable component

--- components/FormAirtable.tsx
+++ components/FormAirtable.tsx
@@ -2,8 +2,8 @@ export default function FormAirtable() {
     return (
         <section className="relative py-12 md:py-20 px-4 sm:px-6 bg-[#f5f5f5] ">
             <div className="max-w-sm md:max-w-4xl mx-auto">
-                <script src="https://static.airtable.com/js/embed/embed_snippet_v1.js"></script>
-                <iframe className="airtable-embed airtable-dynamic-height" src="https://airtable.com/embed/appudjkzcwnIO9ch9/shrwfLcnh19o9jTfr?backgroundColor=red" frameBorder="0" width="100%" height="3970" style={{ background: "transparent", border: "1px solid #ccc" }}></iframe>
+                <h2 className="h2 pb-12 md:pb-16 text-center text-[#637ee0] " data-aos="zoom-y-out">Let's Apply</h2>
+                <iframe className="airtable-embed" src="https://airtable.com/embed/appudjkzcwnIO9ch9/shrwfLcnh19o9jTfr?backgroundColor=red" frameBorder="0" width="100%" height="533" style={{ background: "transparent", border: "1px solid #ccc" }}></iframe>
             </div>
         </section>
     )
