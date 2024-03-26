399d43697c7fd9b6fbb4eb9aae3e495f29a4eac0
Sergio Rey <reysergio383@gmail.com>
2024-03-26 14:46:46 -0500

    Add new images and mentors, implement "Show More" functionality

--- components/Mentors.tsx
+++ components/Mentors.tsx
@@ -1,19 +1,39 @@
+'use client'
 
 import Profile from "@/components/ui/profile"
 import MentorsData from "@/data/mentors"
+import { Speaker } from "@/models/speaker"
+import { useState, useEffect } from "react"
 
 export default function Mentors() {
+    const [showMore, setShowMore] = useState<boolean>(false)
+    const [mentors, setMentors] = useState<Speaker[]>(MentorsData.slice(0, 8))
+
+    useEffect(() => {
+        if (showMore) {
+            setMentors(MentorsData)
+        } else {
+
+        }
+
+    }, [showMore])
+
     return (
         <section className="relative text-center py-12 md:py-20 px-4 sm:px-6 bg-[#f5f5f5]">
             <div className="max-w-4xl mx-auto">
                 <h2 className="h2 pb-12 md:pb-16" data-aos="zoom-y-out">Mentors</h2>
-                <div className="mx-auto flex gap-8 flex-wrap">
+                <div className="mx-auto grid grid-cols-[repeat(auto-fill,minmax(220px,1fr))]">
                     {
-                        MentorsData.map((speaker, index) => (
-                            <Profile key={index} profile={speaker} />
+                        mentors.map((mentor, index) => (
+                            <Profile key={index} profile={mentor} />
                         ))
                     }
                 </div>
+                {
+                    !showMore && (
+                        <button className="rounded-3xl btn text-[#637EE0] bg-white border-[#637EE0] hover:text-white hover:bg-[#637EE0] focus:bg-[#3F61AA] focus:text-white duration-150 w-full mb-4 px-14 sm:w-auto sm:mb-0" onClick={() => setShowMore(true)}>Show More</button>
+                    )
+                }
             </div>
         </section>
     )
--- data/mentors.ts
+++ data/mentors.ts
@@ -6,12 +6,25 @@ import laura_gonzalez_profile from "@/public/images/mentors/laura_gonzalez_profi
 import daniela_alvarez_profile from "@/public/images/mentors/daniela_alvarez_profile.png"
 import javier_villamizar_profile from "@/public/images/mentors/javier_villamizar_profile.png"
 import ana_cristina_gadala_profile from "@/public/images/mentors/ana_cristina_gadala_profile.png"
+import cristina_kenna_profile from "@/public/images/mentors/cristina_kenna_profile.png"
+import freddy_vega_profile from "@/public/images/mentors/freddy_vega_profile.png"
+import christian_van_der_henst_profile from "@/public/images/mentors/christian_van_der_henst_profile.png"
+import karla_berman_profile from "@/public/images/mentors/karla_berman_profile.png"
+import eyal_shats_profile from "@/public/images/mentors/eyal_shats_profile.png"
+import adrian_garcia_aranyos_profile from "@/public/images/mentors/adrian_garcia_aranyos_profile.png"
+import sarah_alsaleh_profile from "@/public/images/mentors/sarah_alsaleh_profile.png"
 
 import mexOpp from "@/public/images/startups/mexOpp.png"
 import stays from "@/public/images/startups/stays.jpeg"
 import nazca from "@/public/images/startups/nazca.png"
 import softbank from "@/public/images/startups/softbank.png"
 import qed_investors from "@/public/images/startups/qed_investors.jpeg"
+import platzi from "@/public/images/startups/platzi.png"
+import ignia from "@/public/images/startups/ignia.png"
+import shark_tank from "@/public/images/startups/shark_tank.png"
+import simpli_route from "@/public/images/startups/simpli_route.jpeg"
+import endeavorglobal from "@/public/images/startups/endeavorglobal.jpeg"
+import outliersvc from "@/public/images/startups/outliersvc.jpeg"
 
 const Mentors: Speaker[] = [
     {
@@ -20,8 +33,8 @@ const Mentors: Speaker[] = [
         srcImageProfile: diego_galvez_profile,
         srcImageStartUp: mexOpp,
         imageStartUpSize: {
-            height: 60,
-            width: 60
+            height: 70,
+            width: 120
         },
     },
     {
@@ -35,13 +48,33 @@ const Mentors: Speaker[] = [
         },
     },
     {
+        name: 'Freddy Vega',
+        description: 'Founder and CEO',
+        srcImageProfile: freddy_vega_profile,
+        srcImageStartUp: platzi,
+        imageStartUpSize: {
+            height: 60,
+            width: 110
+        },
+    },
+    {
         name: 'Daniela Alvarez',
         description: 'Angel Investments',
         srcImageProfile: daniela_alvarez_profile,
         srcImageStartUp: nazca,
         imageStartUpSize: {
             height: 60,
-            width: 60
+            width: 140
+        },
+    },
+    {
+        name: 'Christian Van Der Henst',
+        description: 'Co-founder and President',
+        srcImageProfile: christian_van_der_henst_profile,
+        srcImageStartUp: platzi,
+        imageStartUpSize: {
+            height: 60,
+            width: 110
         },
     },
     {
@@ -62,6 +95,57 @@ const Mentors: Speaker[] = [
         imageStartUpSize: {
             height: 50,
             width: 50
+        },
+    },
+    {
+        name: 'Cristina Kenna',
+        description: 'Partner',
+        srcImageProfile: cristina_kenna_profile,
+        srcImageStartUp: ignia,
+        imageStartUpSize: {
+            height: 60,
+            width: 110
+        },
+    },
+
+    {
+        name: 'Karla Berman',
+        description: 'Shark Tank México',
+        srcImageProfile: karla_berman_profile,
+        srcImageStartUp: shark_tank,
+        imageStartUpSize: {
+            height: 60,
+            width: 60
+        },
+    },
+    {
+        name: 'Eyal Shats',
+        description: 'Co-founder and CSO',
+        srcImageProfile: eyal_shats_profile,
+        srcImageStartUp: simpli_route,
+        imageStartUpSize: {
+            height: 60,
+            width: 60
+        },
+    },
+    {
+        name: 'Adrian Garcia-Aranyos',
+        description: 'Global President',
+        srcImageProfile: adrian_garcia_aranyos_profile,
+        srcImageStartUp: endeavorglobal,
+        imageStartUpSize: {
+            height: 60,
+            width: 60
+        },
+    },
+    {
+        name: 'Sarah AlSaleh',
+        description: 'General Partner',
+        srcImageProfile: sarah_alsaleh_profile,
+        srcImageStartUp: outliersvc,
+        imageStartUpSize: {
+            height: 60,
+            width: 60
         },
     },
 ]
