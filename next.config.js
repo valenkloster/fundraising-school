7f3511f1c43577e1d8886e43f27b7aa2dd5b6005
Sergio Rey <reysergio383@gmail.com>
2024-04-27 02:15:29 -0500

    Define environment variables in next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    LINKEDIN_CLIENT_ID: string,
+    LINKEDIN_CLIENT_SECRET: string,
+    NEXTAUTH_SECRET: string,
+    NEXTAUTH_URL: string,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
