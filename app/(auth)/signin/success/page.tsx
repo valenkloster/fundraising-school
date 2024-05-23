89377cc81a5b60e42f893d846123e0853fe87d85
valenkloster <valekloster18@gmail.com>
2024-05-23 18:30:11 -0300

    add buttons login LinkedIn

--- app/(auth)/signin/success/page.tsx
+++ app/(auth)/signin/success/page.tsx
@@ -29,7 +29,13 @@ export default async function Page() {
           quality={100}
           alt={`Linkedin Profile Picture of ${data?.user?.name}`}
         />
-        <h3 className="h3">{data?.user?.name}</h3>
+        <h3 className="h3 pb-24">{data?.user?.name}</h3>
+        <button className="btn flex h-11 w-full max-w-[335px] items-center gap-[6px] rounded-3xl bg-fsPurple px-4 text-white">
+          Continue
+        </button>
+        <button className="btn mb-64 flex h-11 w-full max-w-[335px] items-center gap-[6px] rounded-3xl border-fsPurple bg-white px-4 text-fsPurple">
+          Use a different account
+        </button>
       </div>
       <div className=""></div>
     </div>
