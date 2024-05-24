8ed4ecfc1cf734eb07d76a3e680574f413fbf06c
valenkloster <valekloster18@gmail.com>
2024-05-24 00:18:38 -0300

    added basic layout vc_list

---
+++ app/(vc_list)/vc_list/layout.tsx
@@ -0,0 +1,54 @@
+/* eslint-disable tailwindcss/classnames-order */
+import React from 'react';
+import Navbar from '@/components/vc_list/navbar';
+import Logo from '@/components/ui/logo';
+import Image from 'next/image';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { redirect } from 'next/navigation';
+import { Session, getServerSession } from 'next-auth';
+
+function existsUser(data: Session | null): boolean {
+  if (data && data.user && data.user.name && data.user.image) {
+    return true;
+  }
+
+  return false;
+}
+
+export default async function VcListLayout({ children }: { children: React.ReactNode }) {
+  const data = await getServerSession(authOptions);
+
+  if (!existsUser(data)) {
+    redirect('/signin');
+  }
+
+  return (
+    <div className="flex">
+      <nav className="flex h-screen w-[256px] flex-col justify-between">
+        <div className="w-full">
+          <div className="w-3/4 pl-10 pt-12">
+            <Logo />
+          </div>
+          <nav className="flex flex-col p-4">
+            <Navbar />
+          </nav>
+        </div>
+        <div className="mb-10 flex flex-col items-center">
+          <Image
+            src={data?.user?.image as string}
+            className="rounded-full"
+            width={90}
+            height={100}
+            quality={80}
+            alt={`Linkedin Profile Picture of ${data?.user?.name}`}
+          />
+          <h3 className="text-base">{data?.user?.name}</h3>
+          <a href="#" className="text-sm text-[#6F7787FF]">
+            Cerrar Sesión
+          </a>
+        </div>
+      </nav>
+      <main className="grow">{children}</main>
+    </div>
+  );
+}
